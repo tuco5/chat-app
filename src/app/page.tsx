4e0f9550963fa0ed9cce29c3eec0cf2259dd0971
tuco5 <tudor.comparan@gmail.com>
2025-05-27 17:28:15 -0600

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className="min-h-screen flex flex-col items-center">
+      <section className="text-center max-w-2xl flex flex-col items-center mt-20 gap-4">
+        <h1 className="md:text-5xl text-4xl font-bold mb-4">
+          Connect Instantly. <br />
+          Chat Seamlessly.
+        </h1>
+        <p className="text-lg mb-8 px-4">
+          A blazing-fast chat app built with Next.js and Socket.io — experience
+          real-time messaging like never before.
+        </p>
+        <Button className="text-xl py-5 px-6 bg-blue-600 text-white hover:bg-blue-700 transition-colors duration-300">
+          Get Started 🚀
+        </Button>
+      </section>
+    </main>
+  );
 }
--- src/components/layout/sellia-logo.tsx
+++ src/components/layout/sellia-logo.tsx
@@ -3,24 +3,26 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <a
-      className="flex flex-col items-center justify-center gap-1 cursor-pointer"
+      className="flex flex-col items-center justify-center cursor-pointer"
       href="https://docs.google.com/document/d/1NpeteBEWAG8Q3rHnWvsl00yOnUflLNceTitfstbY6OE/edit?usp=sharing"
       target="_blank"
       title="Sellia Code Challenge"
     >
-      <div className="flex gap-1 items-baseline italic font-bold text-3xl">
+      <div className="flex gap-1 items-baseline">
         <Image
           src="/logo.png"
           alt="Sellia Logo"
-          height={50}
-          width={50}
-          className="h-10 w-10 flex-shrink-0"
+          height={24}
+          width={24}
+          className="h-6 w-6 lg:h-10 lg:w-10 flex-shrink-0"
           loading="eager"
           priority
         />
-        <p>sellia</p>
+        <p className=" italic font-bold text-2xl lg:text-3xl">sellia</p>
       </div>
-      <p className="text-sm font-mono text-muted-foreground">code challenge</p>
+      <p className="text-xs lg:text-sm font-mono text-muted-foreground">
+        code challenge
+      </p>
     </a>
   );
 }
