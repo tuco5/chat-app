bfee4511ac697ee51b7a094c8eff047ec10a496b
tuco5 <tudor.comparan@gmail.com>
2025-05-28 08:21:21 -0600

    remove header  and footer from dashboard

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import Header from "./_components/header";
+import Footer from "./_components/footer";
+
+export const metadata: Metadata = {
+  title: "Sellia | Chat App",
+  description: "Sellia Code Challenge",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+      <Header />
+      <div className="min-h-[85vh]">{children}</div>
+      <Footer />
+    </main>
+  );
+}
