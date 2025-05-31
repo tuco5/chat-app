932b4049b52adf1ff47a9b935000bff75d999b45
Tudor Comparan <tudor.comparan@gmail.com>
2025-05-31 13:14:41 -0600

    Update footer.tsx

--- src/app/(landing)/_components/footer.tsx
+++ src/app/(landing)/_components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-between items-center h-25 border-t border-muted bg-sidebar/60 mt-auto">
-      <div className="flex flex-col md:flex-row md:items-center items-start gap-2 p-2">
+    <footer className="flex justify-between items-center h-25 border-t border-muted bg-sidebar/60 mt-auto p-2">
+      <div className="flex flex-col md:flex-row md:items-center items-start gap-2">
         <p>&copy; 2025 Sellia.</p>
         <p>All rights reserved.</p>
       </div>
